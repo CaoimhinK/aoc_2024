d97728a403ce6ce730cf6d3ad7e4183814b9abea
Kevin Donnelly <kd@neopoly.de>
2024-12-24 00:33:12 +0100

    Refactor

--- 23/solve.rb
+++ 23/solve.rb
@@ -1,92 +1,50 @@
 # frozen_string_literal: true
 
-input = open('./input.txt').read
-
-##
-class Node
-  attr_reader :name, :connections
-
-  def initialize(name)
-    @name = name
-    @connections = Set[]
-  end
-
-  def <<(node)
-    @connections << node
-  end
-
-  def each_conn(&block)
-    @connections.each(&block)
-  end
-
-  def inspect
-    "#{@name}: #{@connections.map(&:name)}"
-  end
-end
-
-connections = input.split("\n").map { |row| row.split('-') }
-
-nodes = {}
-connections.each do |conn|
-  one, two = conn
-
-  node_one = nodes[one]
-  node_two = nodes[two]
-
-  nodes[one] = Node.new(one) unless node_one
-  nodes[two] = Node.new(two) unless node_two
-
-  node_one = nodes[one]
-  node_two = nodes[two]
-
-  node_one << node_two
-  node_two << node_one
-end
-
-triangles = Set.new
-
-nodes.each_key do |key|
-  node = nodes[key]
-  node.each_conn do |c_node|
-    c_node.each_conn do |c_node2|
-      c_node2.each_conn do |candidate|
-        triangles << Set[node.name, c_node.name, c_node2.name] if candidate.name == node.name
-      end
-    end
-  end
-end
-
-triangles = triangles.filter { |set| set.any? { |a| a.start_with? 't' } }
-
-puts "Part 1: #{triangles.size}"
-
-connections = input.split("\n").map { |row| row.split('-') }
-
-nodes = {}
-connections.each do |conn|
-  one, two = conn
-
-  node_one = nodes[one]
-  node_two = nodes[two]
-
-  nodes[one] = Node.new(one) unless node_one
-  nodes[two] = Node.new(two) unless node_two
-
-  node_one = nodes[one]
-  node_two = nodes[two]
-
-  node_one << node_two
-  node_two << node_one
-end
-
 ##
 class MaxCliqueFinder
-  def initialize(nodes)
-    @nodes = nodes
+  def initialize(input)
+    connections = parse_connections(input)
+    @nodes = create_nodes(connections)
+  end
+
+  def parse_connections(input)
+    input.split("\n").map { |row| row.split('-') }
+  end
+
+  def create_nodes(connections)
+    connections.each_with_object({}) do |conn, nodes|
+      one, two = conn
+
+      nodes[one] = Node.new(one) unless nodes[one]
+      nodes[two] = Node.new(two) unless nodes[two]
+
+      nodes[one] << two
+      nodes[two] << one
+    end
   end
 
   def neighbours(node_name)
-    @nodes[node_name].connections.map(&:name).to_set
+    @nodes[node_name].connections.to_set
+  end
+
+  def triangles_for_key(key)
+    @nodes[key].each_conn do |c_node|
+      @nodes[c_node].each_conn do |c_node2|
+        @nodes[c_node2].each_conn do |candidate|
+          yield Set[key, c_node, c_node2] if candidate == key
+        end
+      end
+    end
+  end
+
+  def find_triangles
+    tris = @nodes.keys.each_with_object(Set.new) do |key, triangles|
+      triangles_for_key(key) do |triangle|
+        triangles << triangle
+      end
+    end
+
+    tris.filter { |set| set.any? { |a| a.start_with? 't' } }
   end
 
   def bron_kerbosch(r_set, p_set, x_set, c_set)
@@ -112,6 +70,32 @@ class MaxCliqueFinder
   end
 end
 
-finder = MaxCliqueFinder.new(nodes)
+##
+class Node
+  attr_reader :name, :connections
+
+  def initialize(name)
+    @name = name
+    @connections = Set[]
+  end
+
+  def <<(node)
+    @connections << node
+  end
+
+  def each_conn(&block)
+    @connections.each(&block)
+  end
+
+  def inspect
+    "#{@name}: #{@connections.map(&:name)}"
+  end
+end
+
+input = open('./input.txt').read
+
+finder = MaxCliqueFinder.new(input)
+
+puts "Part 1: #{finder.find_triangles.size}"
 
 puts "Part 2: #{finder.find_lan_password}"
